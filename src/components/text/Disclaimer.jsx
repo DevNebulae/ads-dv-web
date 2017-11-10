13e307de0d65a6c5a5eb1974412b92c0f3717bc9
DevNebulae <gamerkeeperson+github@gmail.com>
2017-11-10 10:13:43 +0100

    Add textual disclaimer component

---
+++ src/components/text/Disclaimer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Disclaimer() {
+  return (
+    <article className="disclaimer">
+      Permission is hereby granted to Fontys Hogescholen Eindhoven to grade this
+      assignment by the data visualization teacher of class DS72 (fall semester
+      2017-2018). Permission to show or distribute this software to other
+      students, teachers or other people within Fontys Hogescholen must be
+      explicitly acquired from the original author Ivo Huntjens and clearly
+      mentioned in the showing or distribution of this software.
+    </article>
+  )
+}
