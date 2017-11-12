f18db84f2f10ee02e2ee447be93f45eed5b4b9ba
DevNebulae <gamerkeeperson+github@gmail.com>
2017-11-12 19:34:27 +0100

    Update app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,30 @@
 import { graphql } from "react-apollo"
 import { itemGroupBy } from "./../lib/transforms"
-import Item from "./queries/item"
 import Context from "./text/Context"
 import Disclaimer from "./text/Disclaimer"
 import GraphQLError from "./text/GraphQLError"
 import GraphQLLoading from "./text/GraphQLLoading"
+import Hypotheses from "./text/Hypotheses"
 import Introduction from "./text/Introduction"
+import Item from "./queries/item"
 import PriceChart from "./PriceChart"
 import SupplyDemandChart from "./SupplyDemandChart"
 import React, { Component } from "react"
 
 class App extends Component {
   render() {
+    const styling = {
+      axis: {
+        fontSize: "0.8em"
+      },
+      label: {
+        fontSize: "1.2em"
+      }
+    }
+
+    const height = 720
+    const width = 1680
+    const size = [1680, 720]
     if (this.props.data.loading) return <GraphQLLoading />
     else if (this.props.data.error) return <GraphQLError />
     else {
@@ -21,17 +34,22 @@ class App extends Component {
           <h1>Runescape item pricing</h1>
           <Introduction />
           <Context />
+          <Hypotheses />
           <PriceChart
             data={data}
+            size={size}
+            styling={styling}
             xMap={d => d.timestamp}
             yMap={d => d.sellingPrice}
           />
           <SupplyDemandChart
             data={data}
+            size={size}
+            styling={styling}
             xMap={d => d.timestamp}
-            yMap={d => d.delta}
+            yMap={d => d.sellingPriceDelta}
           />
-          <Disclaimer data={data} />
+          <Disclaimer />
         </div>
       )
     }
